cbfe7835af3959df26a4e6fa7d0049fb37b6ec66
daniellekanda <120112357+daniellekanda@users.noreply.github.com>
2023-03-16 14:43:18 +0000

    Upload from local files

---
+++ Checkoutprogram.py
@@ -0,0 +1,23 @@
+"""
+4 clothes items to order (prices of item stored as object - practice dictionary later)
+starting tally - if order then order tally increases. 
+
+welcome message 
+print shop object to terimnal 
+
+
+items 
+object for basket? which adds new items 
+
+calculate basket - claculates all items in basket and prints 
+print to terminal
+print final 
+
+write to receipt file 
+
+
+
+
+
+
+"""
